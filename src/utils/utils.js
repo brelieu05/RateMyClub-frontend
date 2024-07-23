ef82f55579fef9b66cc1de8845918120a3dc4a8c
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-23 16:38:53 -0700

    Edited baseURL in backend

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Backend = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'https://rate-my-club-backend.vercel.app/',
   withCredentials: true,
 });
 
