09ca13f738a4ba86bffe75a316c4e4cf2fd6686b
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-25 02:15:09 -0700

    Removed comment with localhost

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-// const Backend = axios.create({
-//   baseURL: 'http://localhost:5000',
-//   withCredentials: true,
-// });
-
 const Backend = axios.create({
-  baseURL: 'https://ratemyclub-backend-production.up.railway.app/',
+  baseURL: 'https://ratemyclub-backend-production.up.railway.app',
   withCredentials: true,
 });
 
