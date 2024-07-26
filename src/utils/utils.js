99c219e5e20e80ab174e49e148c8a0d8cb71f6ce
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-25 20:30:48 -0700

    Changed Backend baseURL to railway

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// const Backend = axios.create({
-//   baseURL: 'https://ratemyclub-backend-production.up.railway.app',
-//   withCredentials: true,
-// });
-
 const Backend = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://ratemyclub-backend-production.up.railway.app',
   withCredentials: true,
 });
+
+// const Backend = axios.create({
+//   baseURL: 'http://localhost:5000',
+//   withCredentials: true,
+// });
 
 
 export default Backend;
