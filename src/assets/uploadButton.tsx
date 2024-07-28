edffd802bf446b3119a63b6faa8d19e9ccd0f8bd
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-27 20:27:43 -0700

    Added 's' to generateUploadButton

--- src/assets/uploadButton.tsx
+++ src/assets/uploadButton.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { generateUploadButton } from '@uploadthing/react';
 
 export const UploadButton = generateUploadButton({
-  url: 'http://https://ratemyclub-backend-production.up.railway.app/api/uploadthing',
+  url: 'https://https://ratemyclub-backend-production.up.railway.app/api/uploadthing',
 });
 
