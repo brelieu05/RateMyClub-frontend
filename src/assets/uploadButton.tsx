7e038c57b196984f93029f7025c7e2d7f8a97563
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-27 20:31:07 -0700

    removed extra https://

--- src/assets/uploadButton.tsx
+++ src/assets/uploadButton.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { generateUploadButton } from '@uploadthing/react';
 
 export const UploadButton = generateUploadButton({
-  url: 'https://https://ratemyclub-backend-production.up.railway.app/api/uploadthing',
+  url: 'https://ratemyclub-backend-production.up.railway.app/api/uploadthing',
 });
 
