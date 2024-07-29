2c42edab9cac027db30ec1b7090a71a874c4babd
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2024-07-28 21:01:04 -0700

    Mobile view for Navbar

--- src/assets/Navbar.tsx
+++ src/assets/Navbar.tsx
@@ -1,69 +1,120 @@
 import {
-  Flex,
-  Button,
-  HStack,
-} from '@chakra-ui/react'
-
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import React from 'react';
-import { useAuth } from '../contexts/authContext/authContext';
-import { logout } from '../utils/firebaseAuthUtils'
-import Search from './Search';
-
-export default function Navbar(){
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { userLoggedIn, userData } = useAuth();
-
-    const handleLogout = () => {
-        logout('/', navigate);
-        window.location.reload();
-    }
-
-    return (
-        <>
-                <Flex justifyContent='space-between' p='5' mx='5'>
-                    <HStack>
-                        <Button variant='nav' onClick={() => {
-                            navigate('/');
-                            window.location.reload();
-                        }}>Rate My Club</Button>
-                    </HStack>
-
-                    <HStack>
-                        {location.pathname !== '/' 
-                            && 
-                            <Search width={undefined}/>
-                        }
-                        <Button ><Link to='/Browse'>Browse</Link></Button>
-                            { userData?.role === "admin" ? 
-                                (<Button variant='nav'><Link to='/Reports'>Reports</Link></Button>) : <></>
-                            }
-                    {userLoggedIn ? (
-                        <HStack>
-                            <Button variant='nav' onClick={handleLogout}>
-                                <Link to='/'>Logout</Link>
-                            </Button>
-                        </HStack>
-                    ) : (
-                        <HStack>
-                            {location.pathname !== '/SignUp' && (
-                                <Button variant='nav'>
-                                    <Link to='/SignUp'>Sign Up</Link>
-                                </Button>
-                            )}
-                            {location.pathname !== '/Login' && (
-                                <Button variant='nav'>
-                                    <Link to='/Login'>Login</Link>
-                                </Button>
-                            )}
-                        </HStack>
-                    )}
-                    </HStack>
-
-                    
-                </Flex>
-                
-        </>
-    );
-}
+    Flex,
+    Button,
+    HStack,
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Box,
+    IconButton,
+    VStack
+  } from '@chakra-ui/react'
+  import { HamburgerIcon } from '@chakra-ui/icons'
+  import { Link, useLocation, useNavigate } from 'react-router-dom';
+  import React from 'react';
+  import { useAuth } from '../contexts/authContext/authContext';
+  import { logout } from '../utils/firebaseAuthUtils'
+  import Search from './Search';
+  
+  export default function Navbar() {
+      const location = useLocation();
+      const navigate = useNavigate();
+      const { userLoggedIn, userData } = useAuth();
+      const { isOpen, onOpen, onClose } = useDisclosure();
+  
+      const handleLogout = () => {
+          logout('/', navigate);
+          window.location.reload();
+      }
+  
+      return (
+          <>
+              <Flex justifyContent='space-between' p='5' mx='5' alignItems='center'>
+                  <HStack spacing={{ base: '0', md: '5' }}>
+                      <Button variant='nav' onClick={() => {
+                          navigate('/');
+                          window.location.reload();
+                      }}>Rate My Club</Button>
+                  </HStack>
+  
+                  <Box display={{ base: 'none', md: 'flex' }}>
+                      {location.pathname !== '/' && <Search width={undefined} />}
+                      <Button><Link to='/Browse'>Browse</Link></Button>
+                      {userData?.role === "admin" && (
+                          <Button variant='nav'><Link to='/Reports'>Reports</Link></Button>
+                      )}
+                      {userLoggedIn ? (
+                          <Button variant='nav' onClick={handleLogout}>
+                              <Link to='/'>Logout</Link>
+                          </Button>
+                      ) : (
+                          <>
+                              {location.pathname !== '/SignUp' && (
+                                  <Button variant='nav'>
+                                      <Link to='/SignUp'>Sign Up</Link>
+                                  </Button>
+                              )}
+                              {location.pathname !== '/Login' && (
+                                  <Button variant='nav'>
+                                      <Link to='/Login'>Login</Link>
+                                  </Button>
+                              )}
+                          </>
+                      )}
+                  </Box>
+  
+                  <IconButton
+                      display={{ base: 'flex', md: 'none' }}
+                      icon={<HamburgerIcon />}
+                      onClick={onOpen}
+                      variant='outline'
+                      aria-label='Open Menu'
+                  />
+  
+                  <Drawer isOpen={isOpen} placement='right' onClose={onClose}>
+                      <DrawerOverlay />
+                      <DrawerContent>
+                          <DrawerCloseButton />
+                          <DrawerHeader>Menu</DrawerHeader>
+                          <DrawerBody>
+                              <VStack spacing='24px'>
+                                  <Button variant='nav' onClick={() => {
+                                      navigate('/');
+                                      window.location.reload();
+                                  }}>Rate My Club</Button>
+                                  {location.pathname !== '/' && <Search width={undefined} />}
+                                  <Button><Link to='/Browse'>Browse</Link></Button>
+                                  {userData?.role === "admin" && (
+                                      <Button variant='nav'><Link to='/Reports'>Reports</Link></Button>
+                                  )}
+                                  {userLoggedIn ? (
+                                      <Button variant='nav' onClick={handleLogout}>
+                                          <Link to='/'>Logout</Link>
+                                      </Button>
+                                  ) : (
+                                      <>
+                                          {location.pathname !== '/SignUp' && (
+                                              <Button variant='nav'>
+                                                  <Link to='/SignUp'>Sign Up</Link>
+                                              </Button>
+                                          )}
+                                          {location.pathname !== '/Login' && (
+                                              <Button variant='nav'>
+                                                  <Link to='/Login'>Login</Link>
+                                              </Button>
+                                          )}
+                                      </>
+                                  )}
+                              </VStack>
+                          </DrawerBody>
+                      </DrawerContent>
+                  </Drawer>
+              </Flex>
+          </>
+      );
+  }
+  
